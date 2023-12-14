mdeejay/android_kernel_xiaomi_msm8953-4.9

drivers: video: mdss: Import vince related changes
Fix build >> drivers: video: mdss: Convert to wakeup sources
https://github.com/LineageOS/android_kernel_xiaomi_msm8953/commit/4f7c3d193004feccc8d24e804628e3cb9271a600

Signed-off-by: Rama Bondan Prakoso <ramanarubp@gmail.com>

drivers/video/fbdev/msm/mdss_dsi_status.c
@@ -31,12 +31,16 @@
 #include "mdss_panel.h"
 #include "mdss_mdp.h"
 
+#ifdef CONFIG_PROJECT_VINCE
+#define STATUS_CHECK_INTERVAL_MS 1000
+#else
 #define STATUS_CHECK_INTERVAL_MS 5000
+#endif
 #define STATUS_CHECK_INTERVAL_MIN_MS 50
 #define DSI_STATUS_CHECK_INIT -1
 #define DSI_STATUS_CHECK_DISABLE 1
 
-static uint32_t interval = STATUS_CHECK_INTERVAL_MS;
+uint32_t ESD_interval = STATUS_CHECK_INTERVAL_MS;
 static int32_t dsi_status_disable = DSI_STATUS_CHECK_INIT;
 struct dsi_status_data *pstatus_data;
 
@@ -68,7 +72,7 @@ static void check_dsi_ctrl_status(struct work_struct *work)
 		return;
 	}
 
-	pdsi_status->mfd->mdp.check_dsi_status(work, interval);
+	pdsi_status->mfd->mdp.check_dsi_status(work, ESD_interval);
 }
 
 /*
@@ -91,7 +95,7 @@ irqreturn_t hw_vsync_handler(int irq, void *data)
 
 	if (pstatus_data)
 		mod_delayed_work(system_wq, &pstatus_data->check_status,
-			msecs_to_jiffies(interval));
+			msecs_to_jiffies(ESD_interval));
 	else
 		pr_err("Pstatus data is NULL\n");
 
@@ -176,7 +180,7 @@ static int fb_event_callback(struct notifier_block *self,
 		switch (*blank) {
 		case FB_BLANK_UNBLANK:
 			schedule_delayed_work(&pdata->check_status,
-				msecs_to_jiffies(interval));
+				msecs_to_jiffies(ESD_interval));
 			break;
 		case FB_BLANK_VSYNC_SUSPEND:
 		case FB_BLANK_NORMAL:
@@ -248,7 +252,7 @@ int __init mdss_dsi_status_init(void)
 		return -EPERM;
 	}
 
-	pr_info("%s: DSI status check interval:%d\n", __func__,	interval);
+	pr_info("%s: DSI status check interval:%d\n", __func__,	ESD_interval);
 
 	INIT_DELAYED_WORK(&pstatus_data->check_status, check_dsi_ctrl_status);
 
@@ -265,9 +269,9 @@ void __exit mdss_dsi_status_exit(void)
 	pr_debug("%s: DSI ctrl status work queue removed\n", __func__);
 }
 
-module_param_call(interval, param_set_interval, param_get_uint,
-						&interval, 0644);
-MODULE_PARM_DESC(interval,
+module_param_call(ESD_interval, param_set_interval, param_get_uint,
+						&ESD_interval, 0644);
+MODULE_PARM_DESC(ESD_interval,
 	"Duration in milliseconds to send BTA command for DSI status check");
 
 module_param_call(dsi_status_disable, param_dsi_status_disable, param_get_uint,

kernel/reboot.c
@@ -17,6 +17,10 @@
 #include <linux/syscore_ops.h>
 #include <linux/uaccess.h>
 
+#ifdef CONFIG_PROJECT_VINCE
+bool pullDownReset = false;
+#endif
+
 /*
  * this indicates whether you can reboot with ctrl-alt-del: the default is yes
  */
@@ -312,6 +316,14 @@ SYSCALL_DEFINE4(reboot, int, magic1, int, magic2, unsigned int, cmd,
 		cmd = LINUX_REBOOT_CMD_HALT;
 
 	mutex_lock(&reboot_mutex);
+
+#ifdef CONFIG_PROJECT_VINCE
+	if (cmd == LINUX_REBOOT_CMD_RESTART || cmd == LINUX_REBOOT_CMD_POWER_OFF ||
+		cmd == LINUX_REBOOT_CMD_RESTART2) {
+		pullDownReset = true;
+	}
+#endif
+
 	switch (cmd) {
 	case LINUX_REBOOT_CMD_RESTART:
 		kernel_restart(NULL);

dib12/kernel_xiaomi_msm8953

drivers/video/fbdev/msm/mdss_dsi_status.c
@@ -31,12 +31,16 @@
 #include "mdss_panel.h"
 #include "mdss_mdp.h"
 
+#ifdef CONFIG_MACH_XIAOMI_VINCE
+#define STATUS_CHECK_INTERVAL_MS 1000
+#else
 #define STATUS_CHECK_INTERVAL_MS 5000
+#endif
 #define STATUS_CHECK_INTERVAL_MIN_MS 50
 #define DSI_STATUS_CHECK_INIT -1
 #define DSI_STATUS_CHECK_DISABLE 1
 
-static uint32_t interval = STATUS_CHECK_INTERVAL_MS;
+uint32_t ESD_interval = STATUS_CHECK_INTERVAL_MS;
 static int32_t dsi_status_disable = DSI_STATUS_CHECK_INIT;
 struct dsi_status_data *pstatus_data;
 
@@ -68,7 +72,7 @@ static void check_dsi_ctrl_status(struct work_struct *work)
 		return;
 	}
 
-	pdsi_status->mfd->mdp.check_dsi_status(work, interval);
+	pdsi_status->mfd->mdp.check_dsi_status(work, ESD_interval);
 }
 
 /*
@@ -91,7 +95,7 @@ irqreturn_t hw_vsync_handler(int irq, void *data)
 
 	if (pstatus_data)
 		mod_delayed_work(system_wq, &pstatus_data->check_status,
-			msecs_to_jiffies(interval));
+			msecs_to_jiffies(ESD_interval));
 	else
 		pr_err("Pstatus data is NULL\n");
 
@@ -176,7 +180,7 @@ static int fb_event_callback(struct notifier_block *self,
 		switch (*blank) {
 		case FB_BLANK_UNBLANK:
 			schedule_delayed_work(&pdata->check_status,
-				msecs_to_jiffies(interval));
+				msecs_to_jiffies(ESD_interval));
 			break;
 		case FB_BLANK_VSYNC_SUSPEND:
 		case FB_BLANK_NORMAL:
@@ -248,7 +252,7 @@ int __init mdss_dsi_status_init(void)
 		return -EPERM;
 	}
 
-	pr_info("%s: DSI status check interval:%d\n", __func__,	interval);
+	pr_info("%s: DSI status check interval:%d\n", __func__,	ESD_interval);
 
 	INIT_DELAYED_WORK(&pstatus_data->check_status, check_dsi_ctrl_status);
 
@@ -265,9 +269,9 @@ void __exit mdss_dsi_status_exit(void)
 	pr_debug("%s: DSI ctrl status work queue removed\n", __func__);
 }
 
-module_param_call(interval, param_set_interval, param_get_uint,
-						&interval, 0644);
-MODULE_PARM_DESC(interval,
+module_param_call(ESD_interval, param_set_interval, param_get_uint,
+						&ESD_interval, 0644);
+MODULE_PARM_DESC(ESD_interval,
 	"Duration in milliseconds to send BTA command for DSI status check");
 
 module_param_call(dsi_status_disable, param_dsi_status_disable, param_get_uint,

kernel/reboot.c
@@ -17,6 +17,10 @@
 #include <linux/syscore_ops.h>
 #include <linux/uaccess.h>
 
+#ifdef CONFIG_MACH_XIAOMI_VINCE
+bool pullDownReset = false;
+#endif
+
 /*
  * this indicates whether you can reboot with ctrl-alt-del: the default is yes
  */
@@ -312,6 +316,14 @@ SYSCALL_DEFINE4(reboot, int, magic1, int, magic2, unsigned int, cmd,
 		cmd = LINUX_REBOOT_CMD_HALT;
 
 	mutex_lock(&reboot_mutex);
+
+#ifdef CONFIG_MACH_XIAOMI_VINCE
+	if (cmd == LINUX_REBOOT_CMD_RESTART || cmd == LINUX_REBOOT_CMD_POWER_OFF ||
+		cmd == LINUX_REBOOT_CMD_RESTART2) {
+		pullDownReset = true;
+	}
+#endif
+
 	switch (cmd) {
 	case LINUX_REBOOT_CMD_RESTART:
 		kernel_restart(NULL);